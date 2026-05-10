090cbe718bb7bdb4ea28224a984bc6c1e9a03220
Jandres25 <sc.jose.meneces.l@upds.net.bo>
2026-05-09 21:34:54 -0400

    chore(cleanup): eliminar delegators, Services y view_helpers reemplazados

--- app/Controller/Auth/update_password.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/AuthController.php';
-
-(new App\Controller\Auth\AuthController($connection))->resetPassword();
--- app/Controller/User/create.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/UserController.php';
-
-(new App\Controller\User\UserController($connection))->create();
